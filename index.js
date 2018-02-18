991c5a9674df46d0d78cd271092f735d394ce937
bruno refos <brunesrefuso2@gmail.com>
2018-02-18 16:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function updateObjectWithKeyAndValue (object, key, value)
   return Object.assign({}, key, value);
 }
 
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value)
+{
+  
+}
